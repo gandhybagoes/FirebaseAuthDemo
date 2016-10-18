08f672a6761144b30529340af9f307508efe79d8
toshiba <gbagoescahyono@gmail.com>
2016-10-19 01:14:18 +0700

    add data in firebase

--- app/src/main/java/id/sch/smktelkom_mlg/learn/firebaseauthdemo/ProfileActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/firebaseauthdemo/ProfileActivity.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -16,6 +20,11 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
     private TextView textViewUserEmail;
     private Button buttonLogout;
+
+    private DatabaseReference databaseReference;
+
+    private EditText editTextName, editTextAddress;
+    private Button buttonSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,11 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             finish();
             startActivity(new Intent(this, LoginActivity.class));
         }
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        editTextAddress = (EditText) findViewById(R.id.editTextAddress);
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        buttonSave = (Button) findViewById(R.id.buttonSave);
+
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
         textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
@@ -36,6 +50,19 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         buttonLogout = (Button) findViewById(R.id.buttonLogout);
 
         buttonLogout.setOnClickListener(this);
+        buttonSave.setOnClickListener(this);
+    }
+
+    private void saveUserInformation() {
+        String name = editTextName.getText().toString().trim();
+        String add = editTextAddress.getText().toString().trim();
+
+        UserInformation userInformation = new UserInformation(name, add);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        databaseReference.child(user.getUid()).setValue(userInformation);
+        Toast.makeText(this, "Information Saved...", Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -44,6 +71,9 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             firebaseAuth.signOut();
             finish();
             startActivity(new Intent(this, LoginActivity.class));
+        }
+        if (view == buttonSave) {
+            saveUserInformation();
         }
     }
 }
---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/firebaseauthdemo/UserInformation.java
@@ -0,0 +1,19 @@
+package id.sch.smktelkom_mlg.learn.firebaseauthdemo;
+
+/**
+ * Created by toshiba on 19/10/2016.
+ */
+
+public class UserInformation {
+    public String name;
+    public String address;
+
+    public UserInformation() {
+
+    }
+
+    public UserInformation(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
